5552edaabcfa755fa7a90d6343bfd29e177296ea
saraguerreroc <smicaelagc@gmail.com>
2022-05-27 22:46:24 +0200

    Assignment completed

---
+++ main.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+profile = "default"
+region = "us-east-1"
+}
+resource "aws_instance" "my-instance" {
+ami = var.myami
+instance_type = var.instance_type
+}
+terraform {
+backend "s3" {
+bucket = "awsassignmentcloudc1"
+key = "path/to/my/key"
+region = "us-east-1"
+}
+}
